2786e479a8c8ab4ba3f6c67c06a38dc9c9743e17
imt-eiaz <imteiaz@gmail.com>
2025-09-25 23:38:58 +0100

    17. Conditional Rendering With Ternaries

--- src/index.js
+++ src/index.js
@@ -74,12 +74,14 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later</p>
       )}
     </main>
   );
@@ -103,7 +105,7 @@ function Pizza(props) {
 function Footer() {
   const hour = new Date().getHours();
   const openHour = 12;
-  const closeHour = 23;
+  const closeHour = 22;
 
   const isOpen = hour >= openHour && hour <= closeHour;
   // console.log(isOpen);
@@ -113,11 +115,15 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open untill {closeHour}:00. Come visit us or order online</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We are happy to welcome you between {openHour}:00 and {closeHour}:00
+        </p>
       )}
 
       {/* {hour >= openHour && hour <= closeHour
