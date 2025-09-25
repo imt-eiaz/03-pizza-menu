63e053a55e865049b278eaefe0fc15eb53a55f21
imt-eiaz <imteiaz@gmail.com>
2025-09-25 11:36:53 +0100

    8. JavaScript Logic in Components

--- src/index.js
+++ src/index.js
@@ -73,6 +73,16 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 22;
+
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) alert("We are currently open!");
+  // else alert("Sorry we are closed");
+
   return (
     <footer>{new Date().toLocaleTimeString()} "We're currently open."</footer>
   );
