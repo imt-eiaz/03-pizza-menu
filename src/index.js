ce4858aab67a9bdf873a4e44309ae83c1458685e
imt-eiaz <imteiaz@gmail.com>
2025-09-26 08:44:45 +0100

    19. Extracting JSX Into a New Component

--- src/index.js
+++ src/index.js
@@ -106,36 +106,33 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 20;
+  const openHour = 12;
   const closeHour = 22;
 
   const isOpen = hour >= openHour && hour <= closeHour;
-  // console.log(isOpen);
-
-  // if (hour >= openHour && hour <= closeHour) alert("We are currently open!");
-  // else alert("Sorry we are closed");
-
-  // if (!isOpen) return <p>Closed </p>;
 
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're open untill {closeHour}:00. Come visit us or order online</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We are happy to welcome you between {openHour}:00 and {closeHour}:00
         </p>
       )}
-
-      {/* {hour >= openHour && hour <= closeHour
-        ? "We are currently open"
-        : "We are currently closed"} */}
     </footer>
   );
-  // return React.createElement("footer", null, "We're currently open.");
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We're open untill {props.closeHour}:00. Come visit us or order online
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
